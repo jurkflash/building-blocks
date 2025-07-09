a2f594ad5f39dd590f378bdddc9e02f929a3df6b
Wayne <jurkflash@hotmail.com>
2025-07-10 00:13:01 +0800

    🗑️ Remove TenantEntity class and related code

--- src/Pokok.BuildingBlocks.MultiTenancy/TenantEntity.cs
+++ src/Pokok.BuildingBlocks.MultiTenancy/TenantEntity.cs
@@ -1,12 +1,14 @@
-﻿namespace Pokok.BuildingBlocks.MultiTenancy
-{
-    public abstract class TenantEntity : Entity, ITenantScoped
-    {
-        public TenantId TenantId { get; protected set; }
+﻿using Pokok.BuildingBlocks.Domain.Abstractions;
 
-        protected TenantEntity(TenantId tenantId)
-        {
-            TenantId = tenantId ?? throw new ArgumentNullException(nameof(tenantId));
-        }
-    }
+namespace Pokok.BuildingBlocks.MultiTenancy
+{
+    //public abstract class TenantEntity : EntityId, ITenantScoped
+    //{
+    //    public TenantId TenantId { get; protected set; }
+
+    //    protected TenantEntity(TenantId tenantId)
+    //    {
+    //        TenantId = tenantId ?? throw new ArgumentNullException(nameof(tenantId));
+    //    }
+    //}
 }
