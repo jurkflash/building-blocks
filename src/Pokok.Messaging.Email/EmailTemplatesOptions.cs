b85e704cac1301aacd5efad41e79b7375a395ad7
Wayne <jurkflash@hotmail.com>
2025-07-27 23:05:08 +0800

    :sparkles: Add Messaging.Email

---
+++ src/Pokok.Messaging.Email/EmailTemplatesOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Pokok.Messaging.Email
+{
+    public class EmailTemplatesOptions
+    {
+        public const string SectionName = "EmailTemplates";
+        public UserRegisteredConfirmationOptions UserRegisteredConfirmationOptions { get; set; }
+    }
+}
---
+++ src/Pokok.Messaging.Email/SimpleTemplateRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Pokok.Messaging.Email
+{
+    public class SimpleTemplateRenderer : ITemplateRenderer
+    {
+        private readonly EmailTemplatesOptions _templates;
+
+        public SimpleTemplateRenderer(IOptions<EmailTemplatesOptions> options)
+        {
+            _templates = options.Value;
+        }
+
+        public (string Subject, string Body) Render(EmailTemplateKey templateKey, object model)
+        {
+            var templateOptions = templateKey switch
+            {
+                EmailTemplateKey.UserRegisteredConfirmation => _templates.UserRegisteredConfirmationOptions,
+                _ => throw new ArgumentOutOfRangeException(nameof(templateKey), templateKey, null)
+            };
+
+            string subject = templateOptions.Subject;
+            string body = templateOptions.Body;
+
+            foreach (var prop in model.GetType().GetProperties())
+            {
+                var value = prop.GetValue(model)?.ToString() ?? string.Empty;
+                body = body.Replace($"{{{prop.Name}}}", value);
+            }
+
+            return (subject, body);
+        }
+    }
+}
