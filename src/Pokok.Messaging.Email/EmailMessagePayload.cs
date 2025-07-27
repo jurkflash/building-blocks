e98ffaf6d39be5d32ed25c50da49f95c81f31a01
Wayne <jurkflash@hotmail.com>
2025-07-27 23:42:44 +0800

    Add EmailMessagePayload

---
+++ src/Pokok.Messaging.Email/EmailMessagePayload.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokok.Messaging.Email
+{
+    public class EmailMessagePayload
+    {
+        public string ToEmail { get; set; } = default!;
+        public string Subject { get; set; } = default!;
+        public string Body { get; set; } = default!;
+        public string TemplateKey { get; set; } = default!;
+    }
+}
