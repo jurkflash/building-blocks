9c8333b535f62712dc71b2c53845a8a2f5d382b6
Wayne <jurkflash@hotmail.com>
2025-07-28 00:18:43 +0800

    Add Messaging project

---
+++ src/Pokok.BuildingBlocks.Messaging/IMessagePublisher.cs
@@ -0,0 +1,9 @@
+﻿using Pokok.BuildingBlocks.Domain.SharedKernel.Enums;
+
+namespace Pokok.BuildingBlocks.Messaging
+{
+    public interface IMessagePublisher
+    {
+        Task PublishAsync(OutboxMessageType messageType, string payload, CancellationToken cancellationToken);
+    }
+}
