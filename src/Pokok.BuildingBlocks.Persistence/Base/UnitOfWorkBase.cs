51225d7cac44164597647783a0cf08d440a099e4
Wayne <jurkflash@hotmail.com>
2025-08-26 00:01:44 +0800

    UnitOfWork with event dispatch

--- src/Pokok.BuildingBlocks.Persistence/Base/UnitOfWorkBase.cs
+++ src/Pokok.BuildingBlocks.Persistence/Base/UnitOfWorkBase.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pokok.BuildingBlocks.Cqrs.Events;
+using Pokok.BuildingBlocks.Domain.Abstractions;
 using Pokok.BuildingBlocks.Persistence.Abstractions;
 
 namespace Pokok.BuildingBlocks.Persistence.Base
@@ -6,15 +8,37 @@ namespace Pokok.BuildingBlocks.Persistence.Base
     public class UnitOfWorkBase : IUnitOfWork
     {
         private readonly DbContext _context;
+        private readonly IDomainEventDispatcher _domainEventDispatcher;
 
-        public UnitOfWorkBase(DbContext context)
+        public UnitOfWorkBase(DbContext context, IDomainEventDispatcher domainEventDispatcher)
         {
             _context = context;
+            _domainEventDispatcher = domainEventDispatcher;
         }
 
-        public Task<int> CompleteAsync(CancellationToken cancellationToken = default)
+        public async Task<int> CompleteAsync(CancellationToken cancellationToken = default)
         {
-            return _context.SaveChangesAsync(cancellationToken);
+            // Save DB changes first
+            var result = await _context.SaveChangesAsync(cancellationToken);
+
+            // Collect domain events from aggregates
+            var aggregates = _context.ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is AggregateRoot<object>) // detect aggregates
+                .Select(e => e.Entity as AggregateRoot<object>)
+                .Where(e => e is not null)
+                .ToList();
+
+            var domainEvents = aggregates
+                .SelectMany(a => a!.DomainEvents)
+                .ToList();
+
+            // Clear them from the aggregates
+            aggregates.ForEach(a => a!.ClearDomainEvents());
+
+            await _domainEventDispatcher.DispatchAsync(domainEvents, cancellationToken);
+
+            return result;
         }
     }
 }
