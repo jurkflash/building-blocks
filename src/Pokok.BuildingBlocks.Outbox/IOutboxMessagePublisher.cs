d8b50ccdb170fa4b20f299ddba72a7b7a96f579e
jurkflash <jurkflash@hotmail.com>
2025-08-27 01:34:03 +0800

    Change to use IMessagePublisher (#4)

--- src/Pokok.BuildingBlocks.Outbox/IOutboxMessagePublisher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pokok.BuildingBlocks.Outbox
-{
-    public interface IOutboxMessagePublisher
-    {
-        Task PublishAsync(string messageType, string payload, CancellationToken cancellationToken);
-    }
-}
