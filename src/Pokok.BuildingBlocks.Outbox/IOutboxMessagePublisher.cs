e1e74a1f4e4d27f65644d5c739aa7aa411198f4d
jurkflash <jurkflash@hotmail.com>
2025-08-27 01:06:48 +0800

    Outbox with hosted service (#3)

---
+++ src/Pokok.BuildingBlocks.Outbox/IOutboxMessagePublisher.cs
@@ -0,0 +1,7 @@
+﻿namespace Pokok.BuildingBlocks.Outbox
+{
+    public interface IOutboxMessagePublisher
+    {
+        Task PublishAsync(string messageType, string payload, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Pokok.BuildingBlocks.Outbox/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Pokok.BuildingBlocks.Outbox
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOutboxProcessor<TDbContext>(
+            this IServiceCollection services,
+            TimeSpan? interval = null)
+            where TDbContext : DbContext
+        {
+            services.AddHostedService(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<OutboxProcessorHostedService<TDbContext>>>();
+                return new OutboxProcessorHostedService<TDbContext>(sp, logger, interval);
+            });
+
+            return services;
+        }
+    }
+}
