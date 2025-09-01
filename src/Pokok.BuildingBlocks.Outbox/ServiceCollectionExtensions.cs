721f07480b0f7b4b036dbaed460b2e783a5fedee
Wayne <jurkflash@hotmail.com>
2025-09-01 23:13:51 +0800

    Add outbox options

--- src/Pokok.BuildingBlocks.Outbox/ServiceCollectionExtensions.cs
+++ src/Pokok.BuildingBlocks.Outbox/ServiceCollectionExtensions.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Pokok.BuildingBlocks.Outbox
 {
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddOutboxProcessor<TDbContext>(
-            this IServiceCollection services,
-            TimeSpan? interval = null)
+            this IServiceCollection services)
             where TDbContext : DbContext
         {
+            services.AddScoped<IOutboxMessageRepository, OutboxMessageRepository>();
+
             services.AddHostedService(sp =>
             {
+                var options = sp.GetRequiredService<IOptions<OutboxOptions>>();
                 var logger = sp.GetRequiredService<ILogger<OutboxProcessorHostedService<TDbContext>>>();
-                return new OutboxProcessorHostedService<TDbContext>(sp, logger, interval);
+
+                return new OutboxProcessorHostedService<TDbContext>(options, sp, logger);
             });
 
             return services;
